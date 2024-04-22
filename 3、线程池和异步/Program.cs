b64fa56c3313ed5853b18788542e7869f1e08550
liuyichao <lyc_sy@outlook.com>
2024-04-23 02:03:46 +0800

    1

--- 3、线程池和异步/Program.cs
+++ 3、线程池和异步/Program.cs
@@ -10,14 +10,16 @@ namespace _3_线程池和异步
     {
         static async Task Main(string[] args)
         {
-            Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+            Console.WriteLine(Thread.CurrentThread.ManagedThreadId);//线程池id：1
             StringBuilder sb = new StringBuilder();
             for (int i = 0; i < 10000; i++)
             {
                 sb.Append("AAAAAAAAAAA");
             }
-            await File.AppendAllTextAsync(@"e:\1.txt",sb.ToString());
+            await File.AppendAllTextAsync(@"e:\1.txt",sb.ToString());//线程池id：4
             Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
         }
+        //由此可见，可以理解为以await 为模块，如果执行时间较长，线程池会被释放处理其他事务。
+        //等处理完后，会换新的程序池处理，线程切换 对于系统来说是一个耗费资源的动作
     }
 }
