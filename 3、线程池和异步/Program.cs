56bffa839026a39fc641e5200e88ae5088d2d8be
liuyichao <lyc_sy@outlook.com>
2024-04-25 22:24:52 +0800

    async

--- 3、线程池和异步/Program.cs
+++ 3、线程池和异步/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Net.Http;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -15,12 +16,14 @@ namespace _3_线程池和异步
             for (int i = 0; i < 10000; i++)
             {
                 sb.Append("AAAAAAAAAAA");
+                Console.WriteLine(sb.ToString());
             }
-            await File.AppendAllTextAsync(@"e:\1.txt",sb.ToString());//线程池id：4
+            Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+            await File.AppendAllTextAsync(@"e:\1.txt",sb.ToString());//线程池id：4，也有一个原因 是File方法内置开辟一个线程池
             Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
         }
         //由此可见，可以理解为以await 为模块，如果执行时间较长，线程池会被释放处理其他事务。
         //等处理完后，会换新的程序池处理，线程切换 对于系统来说是一个耗费资源的动作
-
+      
     }
 }
---
+++ 4、async为什么非必须？/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace _4_async为什么非必须_
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string a =await  ReaadAsync(0);
+            Console.WriteLine(a);
+            string b = await ReaadAsync2(1);
+            Console.WriteLine(b);
+        }
+        static async Task<string> ReaadAsync(int num)
+        {
+            if (num == 0)
+            {
+                string a = await File.ReadAllTextAsync(@"e:\1.txt");
+                return a;
+            }
+
+            else if (num == 1)
+            {
+                return await File.ReadAllTextAsync(@"e:\1.txt");
+            }
+            else
+            {
+                throw new ArgumentException();
+            }
+        }
+        //async 和 await 是组合关键字，如果方法返回值本身 是Task<T>,可以去掉关键字
+        static Task<string> ReaadAsync2(int num)
+        {
+            if (num == 1)
+            {
+                return  File.ReadAllTextAsync(@"e:\1.txt");
+            }
+            else
+            {
+                throw new ArgumentException();
+            }
+        }
+    }
+}
